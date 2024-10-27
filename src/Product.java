d6723aa105c97a34f60211f7ca4ce7d649390497
Vitalii  Komar <4999vk@gmail.com>
2024-10-27 16:15:46 +0100

    price

---
+++ src/Product.java
@@ -0,0 +1,25 @@
+public class Product {
+    public String name;
+    public double price;
+    public double rating;
+    public int stock;
+
+    public Product(String name, double price, double rating, int stock){
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+        this.stock = stock;
+    }
+    public String getName(){
+        return name;
+    }
+    public  double getPrice(){
+        return price;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public int getStock(){
+        return stock;
+    }
+}
