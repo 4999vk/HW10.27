7eafcf15ba973af33d271e0e1af57aca3d537939
Vitalii  Komar <4999vk@gmail.com>
2024-10-27 23:33:42 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
+import java.util.Comparator;
 
 
 
@@ -19,7 +20,47 @@ public class Main {
                 new Product("Cheese",6,4.5,5)
         ));
 
-        print(productList);
+        System.out.println(productList);
+        Scanner scanner = new Scanner(System.in);
+        int selection=-1;
+        while (selection!=0){
+            System.out.println("\n Выбор критерия сортировки или  0 для завершения работы");
+            System.out.println("1 - по названию ");
+            System.out.println("2 - по цене ");
+            System.out.println("3 - по рейтингу");
+            System.out.println("4 - по остатку на складе ");
+            System.out.println("Ваш выбор:");
+            selection = scanner.nextInt();
+            if (selection==0){
+                System.out.println("Выход.");
+                break;
+            }
+            Comparator<Product> comparator = null;
+
+            if (selection==1){
+                comparator = new ComparatorProductByName();
+            }else if (selection==2){
+                comparator = new ComparatorProductByPrice();
+            }else if (selection==3){
+                comparator = new ComparatorProductByRating();
+            }else if (selection==4){
+                comparator = new ComparatorProductByStock();
+            }
+            if (comparator!=null){
+                productList.sort(comparator);
+                System.out.println("Список товара: ");
+                for (Product produkt:productList){
+                    System.out.println(produkt);
+                }
+            }else {
+                System.out.println(" Пробуем еще раз)");
+            }
+        }
+    }
+}
+
+
+       /* print(productList);
         productList.sort(new ComparatorProductByName());
         System.out.println("------------ sort By Name --------------- ");
         print(productList);
@@ -47,3 +88,5 @@ public class Main {
         }
     }
 }
+
+        */
